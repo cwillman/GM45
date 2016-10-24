0c8d78d2eb569ccc2397e485c2b71bdc531662fc
Cameron Willman <Cameron Willman>
2016-10-24 02:32:29 -0700

    point

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,3 +1,10 @@
+var request = require('request');
+var apiOptions = {
+  server : "http://localhost:3000"
+};
+if (process.env.NODE_ENV === 'production') {
+  apiOptions.server = "https://sheltered-shelf-81040.herokuapp.com/";
+}
 /* GET 'home' page */
 module.exports.homelist = function(req, res) { //tags
     res.render('locations-list', {
